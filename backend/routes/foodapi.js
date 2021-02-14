1b091127451c8288a47a9b3da16614065ac1f9e8
Andre Grech <silver.grech@gmail.com>
2021-02-14 22:40:08 +1100

    update food api routing

--- backend/routes/foodapi.js
+++ backend/routes/foodapi.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const {} = require("../controllers/auth");
+const {
+  getFood,
+  getListItems,
+  createListItems,
+  updateItems,
+  deleteItem,
+  deleteAll,
+  addFood,
+  updateFood,
+} = require("../controllers/product");
 // const cleanBody = require("../controllers/middlewares/cleanbody.js");
 const { validateToken } = require("../controllers/middlewares/validateToken");
 
-router.get("getfoods", validateToken, getFood);
+router.get("/getfood", validateToken, getFood);
 
 router.get("/listitems", validateToken, getListItems);
-router.post("/listitems", validateToken, createListItems);
-router.put("/listitems/:id", validateToken, updateItems);
-router.delete("/listitems/:id", validateToken, deleteItems);
 
-router.delete("/removelist", validateToken, removeList);
+router.post("/listitems", validateToken, createListItems);
+
+router.put("/listitems", validateToken, updateItems);
+
+router.delete("/listitems", validateToken, deleteItem);
+
+router.delete("/listitems", validateToken, deleteAll);
+
+router.post("/food", addFood);
+
+router.put("/food", updateFood);
 
 module.exports = router;
