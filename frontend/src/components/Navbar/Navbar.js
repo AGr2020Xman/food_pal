13057d6d227e558d5e01071a82d6180a95defed9
Andre Grech <silver.grech@gmail.com>
2021-02-14 22:41:44 +1100

    refining navbar with mui

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -7,26 +7,10 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import TemporaryDrawer from "../TemporaryDrawer/TemporaryDrawer";
-
 import { Link, useHistory } from "react-router-dom";
 import { useAppContext } from "../../store";
 import { setUserLogout } from "../../actions";
-
-// function Navbar() {
-
-//   return (
-//     <nav className="navbar navbar-expand-lg">
-//       <div
-//         className="collapse navbar-collapse d-flex justify-content-end"
-//         id="navbar1"
-//       >
-//         {state.isAuthenticated ? userLink : loginRegLink}
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
+import { List } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,6 +21,10 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+  },
+  navbarDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`,
   },
 }));
 
@@ -51,176 +39,51 @@ const Navbar = () => {
     history.push("/");
   };
 
-  console.log({ state });
+  // console.log({ state });
 
   const loginRegLink = (
-    <ul className="navbar-nav list-group list-group-horizontal">
-      <li>
-        <Button color="secondary" variant="contained" href="/">
-          Home
-        </Button>
-      </li>
-      <li>
-        <Button color="inherit" variant="contained" href="/signin">
-          Sign in
-        </Button>
-
-        {/* <Link className="mb-1 mr-1 btn btn-sm active" to="/signin">
-                    Sign in
-                </Link> */}
-      </li>
-      <li>
-        <Button color="inherit" variant="contained" href="/signup">
-          Sign up
-        </Button>
-
-        {/* <Link className="btn btn-sm active" to="/signup">
-                    Sign Up
-                </Link> */}
-      </li>
-    </ul>
+    <List className={`${classes.menuButton} ${classes.navbarDisplayFlex}`}>
+      <Button color="primary" variant="contained" href="/">
+        Home
+      </Button>
+      <Button color="secondary" variant="contained" href="/signin">
+        Sign in
+      </Button>
+      <Button color="secondary" variant="contained" href="/signup">
+        Sign up
+      </Button>
+    </List>
   );
   const userLink = (
-    <ul
-      className={`navbar-nav list-group list-group-horizontal ${classes.menuButton}`}
-    >
-      <li>
-        <Button color="inherit" variant="contained" href="/">
-          Home
-        </Button>
-        {/* 
-                <Link className="mb-1 mr-1 btn btn-sm active" to="/">
-                    Home
-                </Link> */}
-      </li>
-      <li>
-        <button
-          className="btn btn-sm active"
-          id="logoutBtn"
-          data-toggle="modal"
-          data-target="#logoutModal"
-          onClick={handleLogOut}
-        >
-          <div>Sign out</div>
-        </button>
-      </li>
-    </ul>
+    <List className={`${classes.menuButton} ${classes.navbarDisplayFlex}`}>
+      <Button color="primary" variant="contained" href="/">
+        Home
+      </Button>
+      <Button color="secondary" variant="contained" onClick={handleLogOut}>
+        Sign out
+      </Button>
+    </List>
   );
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            {/* <MenuIcon /> */}
-            <TemporaryDrawer />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            FoodPal
-          </Typography>
-          {state.isAuthenticated ? userLink : loginRegLink}
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="relative" className={classes.root}>
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          {/* <MenuIcon /> */}
+          <TemporaryDrawer />
+        </IconButton>
+        <Typography variant="h6" className={classes.title} nowrap>
+          FoodPal
+        </Typography>
+        {state.isAuthenticated ? userLink : loginRegLink}
+      </Toolbar>
+    </AppBar>
   );
 };
 
 export default Navbar;
-
-// const Navbar = () => {
-//   return (
-//     <div className="navbar">
-//       <div className="container">
-//         <h2>FoodPal</h2>
-//       </div>
-//     </div>
-//   );
-// };
-
-// import React from "react";
-// // import Navbar from "react-bootstrap/Navbar";
-// import { Link } from 'react-router-dom';
-
-// const Nav = () => {
-//   return (
-//     <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
-//       <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-//       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//       <Navbar.Collapse className="justify-content-end">
-//         <Navbar.Text>
-//           Signed in as: <a href="#login">Mark Otto</a>
-//         </Navbar.Text>
-//       </Navbar.Collapse>
-//     </Navbar>
-//   );
-// };
-
-// import { Link, useHistory } from 'react-router-dom';
-// import { useAppContext } from '../../store';
-// import { useLoginCheck, logout } from '../../utils/setAuthToken';
-// // import './style.css';
-
-// function Navbar() {
-//     const history = useHistory();
-//     const [state, dispatch] = useAppContext();
-
-//     useLoginCheck(dispatch);
-
-//     const handleLogOut = (e) => {
-//         e.preventDefault();
-//         logout(dispatch);
-//         history.push('/');
-//     };
-
-//     const loginRegLink = (
-//         <ul className="navbar-nav list-group list-group-horizontal">
-//             <li>
-//                 <Link className="mb-1 mr-1 btn btn-sm active" to="/login">
-//                     Login
-//                 </Link>
-//             </li>
-//             <li>
-//                 <Link className="btn btn-sm active" to="/register">
-//                     Register
-//                 </Link>
-//             </li>
-//         </ul>
-//     );
-//     const userLink = (
-//         <ul className="navbar-nav list-group list-group-horizontal">
-//             <li>
-//                 <Link className="mb-1 mr-1 btn btn-sm active" to="/">
-//                     Home
-//                 </Link>
-//             </li>
-//             <li>
-//                 <Link className="mb-1 mr-1 btn btn-sm active" to="/dashboard">
-//                     Dashboard
-//                 </Link>
-//             </li>
-//             <li>
-//                 <button
-//                     className="btn btn-sm active"
-//                     id="logoutBtn"
-//                     data-toggle="modal"
-//                     data-target="#logoutModal"
-//                     onClick={handleLogOut}
-//                 >
-//                     <div>Logout</div>
-//                 </button>
-//             </li>
-//         </ul>
-//     );
-//     return (
-//         <nav className="navbar navbar-expand-lg">
-//             <div className="collapse navbar-collapse d-flex justify-content-end" id="navbar1">
-//                 {state.isAuthenticated ? userLink : loginRegLink}
-//             </div>
-//         </nav>
-//     );
-// }
