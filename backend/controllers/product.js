69221ed8e5ffbc31ac84d0655a199c46ea33684c
Andre Grech <silver.grech@gmail.com>
2021-02-14 22:38:49 +1100

    detailing success messages + logs

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,5 +1,5 @@
-import Products from "../models/Products";
-import StoredFood from "../models/StoredFood";
+const Products = require("../models/Products");
+const StoredFood = require("../models/StoredFood");
 // import Perishables from "../modles/Perishables";
 // const Joi = require("joi");
 // const { v4: uuid } = require("uuid");
@@ -9,106 +9,183 @@ exports.getFood = async (req, res) => {
   try {
     const products = await Products.find(); // returns arr of obj
 
-    return res.status(200).json({products});
+    return res.status(200).json({
+      success: true,
+      message: "Products received",
+      products: products,
+    });
   } catch (err) {
     console.log(err);
   }
 };
+
 exports.getListItems = async (req, res) => {
   try {
     const { _id } = req.decoded;
-    const listItems = await StoredFood.find({ owner: _id })// may need to filter query down to also find just the name of the list it belongs to
-    return res.status(200).json({listItems})
+    const listItems = await StoredFood.find({ owner: _id }); // may need to filter query down to also find just the name of the list it belongs to
+    return res.status(200).json({
+      success: true,
+      message: "List items retrieved",
+      listArray: listItems,
+    });
   } catch (err) {
-      console.log(err);
+    console.log(err);
   }
 };
 
 exports.createListItems = async (req, res) => {
-    try {
-        const bulk = req.body
-        StoredFood.insertMany(bulk).then(() => {
-          console.log('Success');
+  try {
+    console.log(req);
+    console.log(req.decoded);
+    const { _id } = req.decoded;
+
+    const bulk = req.body;
+    console.log("pre creating list items", req.body);
+    const alreadyExistingItems = await StoredFood.find({ owner: _id }); //arr of obj
+
+    if (!alreadyExistingItems) {
+      await StoredFood.insertMany(bulk)
+        .then(() => {
+          console.log("Success");
           return res.status(200).json({
             success: true,
-            message: 'List items saved'
-          })
+            message: "List items saved",
+          });
         })
+        .catch((err) => {
+          console.log("insert filtered result", err);
+          res.status(400).json({
+            error: true,
+            message: err,
+          });
+        });
+    } else {
+      console.log("already exists array", alreadyExistingItems);
+      //two arrays - bulk && alreadyExistingItems
+      const delineater = ["existsId"];
 
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({
-          error: true,
-          message: err
+      const result = await bulk
+        .filter((o1) => {
+          return !alreadyExistingItems.some((o2) => {
+            return o1.existsId === o2.existsId;
+          });
         })
+        .map((o) => {
+          return delineater.reduce((newO, existsId) => {
+            newO[existsId] = o[existsId];
+            return newO;
+          }, {});
+        });
+
+      await StoredFood.insertMany(result)
+        .then(() => {
+          console.log("Success");
+          return res.status(200).json({
+            success: true,
+            message: "List items saved",
+          });
+        })
+        .catch((err) => {
+          console.log("insert filtered result", err);
+          res.status(400).json({
+            error: true,
+            message: err,
+          });
+        });
+
+      await bulk
+        .forEach((listObj) => {
+          let filter = { existsId: listObj.existsId };
+          let update = listObj;
+          StoredFood.findOneAndUpdate(filter, update);
+        }) // <filter>, <update>
+        .then(() => {
+          console.log("Success");
+          return res.status(200).json({
+            success: true,
+            message: "List items saved",
+          });
+        })
+        .catch((err) => {
+          console.log("update bulk req.body", err);
+          res.status(400).json({
+            error: true,
+            message: err,
+          });
+        });
     }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.updateItems = async (req, res) => {
-  try {
+  const bulk = req.body; //array of objects
 
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({
-      error: true,
-      message: err
+  bulk
+    .forEach((listObj) => {
+      let filter = { existsId: listObj.existsId };
+      let update = listObj;
+      StoredFood.findOneAndUpdate(filter, update);
     })
-  }
-}
-router.put("/listitems/:id", validateToken, updateItems)
-router.delete("/listitems/:id", validateToken, deleteItems)
+    .then(() => {
+      console.log("Success");
+      return res
+        .status(200)
+        .json({ success: true, message: "List items updated" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({
+        error: true,
+        message: err,
+      });
+    });
+};
 
+exports.deleteItem = async (req, res) => {
+  console.log(req.body);
+  const existsId = req.body.existsId; // or req.body.data._id
+  StoredFood.findByIdAndDelete(existsId, (err) => {
+    if (err) {
+      console.log(`Successful delete of ${existsId}`);
+    }
+  });
+};
 
-router.delete("/removelist", validateToken, removeList);
+exports.deleteAll = async (req, res) => {
+  console.log(req.body);
+  // const { _id } = req.decoded;
+  const _id = req.body._id; // or req.body.data._id
+  StoredFood.deleteMany({ owner: _id }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`List items belonging to ${_id} are deleted`);
+    }
+  });
+};
 
-exports.createListItems = async (req, res) => {};
-exports.addFreshFood = async (req, res) => {};
-exports.addPerishableFood = async (req, res) => {};
-exports.updateFresh = async (req, res) => {};
-exports.updatePerish = async (req, res) => {};
-exports.removeList = async (req, res) => {};
-
-exports.addFreshFood = async (req, res) => {
+exports.addFood = async (req, res) => {
   try {
-    const freshFood = await Products.findOne({ name: req.body.name });
+    const item = req.body;
+    const food = await Products.findOne({ name: item.name });
 
-    if (freshFood) {
+    if (food) {
       return res.status(422).json({
         errors: true,
         message: "item already exists",
       });
     }
-    // name: {
-    //     type: String,
-    //     required: true,
-    //   },
-    //   canRefrigerate: {
-    //     type: Boolean,
-    //     required: true,
-    //   },
-    //   canFreeze: {
-    //     type: Boolean,
-    //     required: true,
-    //   },
-    //   standardExpiry: {
-    //     type: Number,
-    //     required: true,
-    //   },
-    //   fridgeExpiry: {
-    //     type: Number,
-    //     required: false,
-    //   },
-    //   freezerExpiry: {
-    //     type: Number,
-    //     required: false,
-    //   },
-    const newFood = new User({
-      name: req.body.name,
-      canRefrigerate: req.body.canRefrigerate,
-      canFreeze: req.body.canFreeze,
-      standardExpiry: req.body.standardExpiry,
-      fridgeExpiry: req.body.fridgeExpiry,
-      freezerExpiry: req.body.freezerExpiry,
+
+    const newFood = new Products({
+      name: item.name,
+      isFresh: item.isFresh,
+      canRefrigerate: item.canRefrigerate,
+      canFreeze: item.canFreeze,
+      standardExpiry: item.standardExpiry,
+      fridgeExpiry: item.fridgeExpiry,
+      freezerExpiry: item.freezerExpiry,
     });
     await newFood.save();
 
@@ -125,61 +202,4 @@ exports.addFreshFood = async (req, res) => {
   }
 };
 
-exports.addPerishableFood = async (req, res) => {
-  try {
-    const perishableFood = await FreshProduce.findOne({ name: req.body.name });
-
-    if (freshFood) {
-      return res.status(422).json({
-        errors: true,
-        message: "item already exists",
-      });
-    }
-    // name: {
-    //     type: String,
-    //     required: true,
-    //   },
-    //   canRefrigerate: {
-    //     type: Boolean,
-    //     required: true,
-    //   },
-    //   canFreeze: {
-    //     type: Boolean,
-    //     required: true,
-    //   },
-    //   standardExpiry: {
-    //     type: Number,
-    //     required: true,
-    //   },
-    //   fridgeExpiry: {
-    //     type: Number,
-    //     required: false,
-    //   },
-    //   freezerExpiry: {
-    //     type: Number,
-    //     required: false,
-    //   },
-    const newFood = new User({
-      name: req.body.name,
-      canRefrigerate: req.body.canRefrigerate,
-      canFreeze: req.body.canFreeze,
-      standardExpiry: req.body.standardExpiry,
-      fridgeExpiry: req.body.fridgeExpiry,
-      freezerExpiry: req.body.freezerExpiry,
-    });
-    await newFood.save();
-
-    return res.status(200).json({
-      success: true,
-      message: "Add successful",
-    });
-  } catch (error) {
-    console.error("adding new item error", error);
-    return res.status(500).json({
-      error: true,
-      message: "Error adding item",
-    });
-  }
-};
-
-exports.updateFood = () => {};
+exports.updateFood = async (req, res) => {};
