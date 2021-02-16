dfdbb54d22e75b8592cd2c7b22c8034cb49d6d3f
Andre Grech <silver.grech@gmail.com>
2021-02-16 21:13:16 +1100

    fixing public folder?

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
+// import reportWebVitals from "./reportWebVitals";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   // <React.StrictMode>
@@ -12,8 +12,8 @@ ReactDOM.render(
   // </React.StrictMode>,
   document.getElementById("root")
 );
-serviceWorker.unregister();
+// serviceWorker.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
